ee6e364ec2fa747841fea8e660958116febbad37
Hiawe1403 <128429681+Hiawe1403@users.noreply.github.com>
2023-05-05 20:47:42 +0700

    tugas webpro

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Agung</li>
+        <li>Nama Belakang : Peter Praditia</li>
+        <li>Nama Lengkap : Agung Peter Praditia</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="disc">
+        <li> Jalan Karet Pasar Baru Timur</li>
+    </ul>
+    
+    <h4>Tempat,Tanggal Lahir</h4>
+    <ul type="disc">
+        <li>Jakarta,14 Maret 1998</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="disc">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
